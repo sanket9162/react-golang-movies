7dcfcf659891ff54b7d5d3cc2e338719db487c70
sanket9162 <sanketgondhali55@gmail.com>
2025-03-31 21:44:37 +0530

    displaying individual movie

--- go-movies-front-end/src/components/Movie.jsx
+++ go-movies-front-end/src/components/Movie.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+
+
+
 const Movie = () => {
-    return(
+
+        const [movie, setMovies] = useState({});
+        let {id} = useParams();
+
+        useEffect(()=> {
+            let myMovie = {
+                id: 1,
+                title: "Highlander",
+                release_data: "1986-03-07",
+                runtime: 116,
+                mpaa_rating: "R",
+                description: "Some long description"
+            }
+            setMovies(myMovie)
+        }, [id])
+
+        return(
         <>
         <div className="text-center">
-            <h2>Movie</h2>
+            <h2>Movie : {movie.title}</h2>
+            <small><em>{movie.release_data}, {movie.runtime}, minutes, Rated {movie.mpaa_rating}</em></small>
             <hr />
-
+            <p>{movie.description}</p>
         </div>
         </>
     )
