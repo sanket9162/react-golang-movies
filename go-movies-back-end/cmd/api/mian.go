fb8881a692de47e306a6a7e481dcae6fbd5fc692
sanket9162 <sanketgondhali55@gmail.com>
2025-04-01 16:34:58 +0530

    database driver & connecting to postgres

--- go-movies-back-end/cmd/api/mian.go
+++ go-movies-back-end/cmd/api/mian.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -12,6 +13,7 @@ const port = 8080
 type application struct{
 	DSN string
 	Domain string
+	DB *sql.DB
 }
 
 func main(){
@@ -19,15 +21,21 @@ func main(){
 	var app application
 
 	// read from command line 
-	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movie sslmode=disable timezone=UTC connect_timeout=5", "Postgres connention string")
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connention string")
 	flag.Parse()
+
 	//connent to the database
+	conn, err := app.connentToDB()
+	if err != nil{
+		log.Fatal(err)
+	}
+	app.DB = conn
 
 	app.Domain= "example.com"
 	log.Println("Starting application on port", port)
 
 	//start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes() )
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes() )
 	if err != nil {
 		log.Fatal(err)
 	}
