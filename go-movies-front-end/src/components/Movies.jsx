97d86a0ab3b2f2b45c1058957b75158588e1baee
sanket9162 <sanketgondhali55@gmail.com>
2025-04-01 11:13:27 +0530

    connect frontend to backend

--- go-movies-front-end/src/components/Movies.jsx
+++ go-movies-front-end/src/components/Movies.jsx
@@ -6,25 +6,22 @@ const Movies = () => {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        let movieList = [
-            {
-                id: 1,
-                title: "Highlander",
-                release_data: "1986-03-07",
-                runtime: 116,
-                mpaa_rating: "R",
-                description: "Some long description"
-            },
-            {
-                id: 2,
-                title: "Raiders of the Lost Ark",
-                release_data: "1986-03-07",
-                runtime: 116,
-                mpaa_rating: "R",
-                description: "Some long description"
-            },
-        ];
-        setMovies(movieList)
+        const headers = new Headers()
+        headers.append("Content-Type", "application/json");
+
+        const requestOptions = {
+            method: "GET",
+            headers: headers,
+        }
+
+        fetch(`http://localhost:8080/movies`, requestOptions)
+        .then((response)=> response.json())
+        .then((data)=>{
+            setMovies(data)
+        })
+        .catch(err =>{
+            console.log(err)
+        })
     }, [])
     return(
         <>
