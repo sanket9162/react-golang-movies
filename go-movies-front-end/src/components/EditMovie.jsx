35991babd337d9c64b1e4b717f049d06b08edcc2
sanket9162 <sanketgondhali55@gmail.com>
2025-04-05 18:59:16 +0530

    edit movie form

--- go-movies-front-end/src/components/EditMovie.jsx
+++ go-movies-front-end/src/components/EditMovie.jsx
@@ -1,12 +1,72 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useOutletContext, useParams } from "react-router-dom";
+import Input from "./form/Input";
 
 const EditMovie = () => {
+  const navigate = useNavigate();
+  const { jwtToken } = useOutletContext();
 
-    return(
-        <div>
-            <h2>Edit Movie</h2>
-            <hr />
-        </div>
-    )
-}
+  const [error, setError] = useState(null);
+  const [errors, setErrors] = useState([]);
+
+  const hasError = (key) => {
+    return errors.indexOf(key) !== -1;
+  };
+
+  const [movie, setMovie] = useState({
+    id: 0,
+    title: "",
+    release_date: "",
+    runtime: "",
+    mpaa_rating: "",
+    description: "",
+  });
+
+  // get id from the URL
+  let { id } = useParams();
+
+  useEffect(() => {
+    if (jwtToken === "") {
+      navigate("/login");
+      return;
+    }
+  }, [jwtToken, navigate]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChange = () => (event) => {
+    let value = event.target.value;
+    let name = event.target.name;
+    setMovie({
+      ...movie,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div>
+      <h2>Add/Edit Movie</h2>
+      <hr />
+      <pre>{JSON.stringify(movie, null, 3)}</pre>
+
+      <form onSubmit={handleSubmit}>
+        <input type="hidden" name="id" value={movie.id} id="id"></input>
+
+        <Input
+          title={"Title"}
+          className={"form-control"}
+          type={"text"}
+          name={"title"}
+          value={movie.title}
+          onChange={handleChange("title")}
+          errorDiv={hasError("title") ? "text-danger" : "d-none"}
+          errorMsg={"Please enter a title"}
+        />
+      </form>
+    </div>
+  );
+};
 
 export default EditMovie;
