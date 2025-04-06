43b3142d3cd64e39a0c8874b0565125499990fe9
sanket9162 <sanketgondhali55@gmail.com>
2025-04-06 16:54:01 +0530

    Editing  existing movie

--- go-movies-front-end/src/components/EditMovie.jsx
+++ go-movies-front-end/src/components/EditMovie.jsx
@@ -90,6 +90,46 @@ const EditMovie = () => {
         });
     } else {
       // editing an existing movie
+      const headers = new Headers();
+      headers.append("Content-Type", "application/json");
+      headers.append("Authorization", "Bearer " + jwtToken);
+
+      const requestOptions = {
+        method: "GET",
+        headers: headers,
+      };
+
+      fetch(`/admin/movies/${id}`, requestOptions)
+        .then((response) => {
+          if (response.status !== 200) {
+            setError("Invalid response code: " + response.status);
+          }
+          return response.json;
+        })
+        .then((data) => {
+          //fix release date
+          data.movie.release_data = new Date(data.movie.release_data)
+            .toISOString()
+            .split("T")[0];
+
+          const checks = [];
+          data.genres.forEach((g) => {
+            if (data.movie.genres_array.indexOf(g.id) !== -1) {
+              checks.push({ id: g.id, checked: true, genre: g.genre });
+            } else {
+              checks.push({ id: g.id, checked: false, genre: g.genre });
+            }
+          });
+
+          //set state
+          setMovie({
+            ...data.movie,
+            gemres: checks,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }, [id, jwtToken, navigate]);
 
